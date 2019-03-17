15e7e633bb62146ed7be7a6e9c982241ea213824
KoenLemaire <KoenLemaire@users.noreply.github.com>
2019-03-16 19:43:28 -0600

    added animation and tested non zero xh

--- test_bird_stance.m
+++ test_bird_stance.m
@@ -12,7 +12,7 @@ head_mass_rel=.2; % [] proportion of head mass relative to total pigeon mass
 hh_rel=1.05;
 parms.L=1; % [m] leg length
 parms.alpha=.3; % [rad] half leg spread angle
-parms.speed=1; % [m/s] desired walking speed
+parms.speed=2; % [m/s] desired walking speed
 
 % derived parameters
 parms.mp=(1-head_mass_rel)*pigeon_mass; % [kg] pigeon pelvis mass   
@@ -23,7 +23,7 @@ parms.step_time=parms.step_length/parms.speed; % [s] desired step duration
 
 % initial condition (maybe make this a parameter input?? to distinguish
 % from initial guess in an optimation scheme ...??)
-x0 = [.5*pi+parms.alpha; 0; 1*parms.L; 0]; % [phi phid xh xhd] a sample initial state
+x0 = [.5*pi+parms.alpha; -1; 1*parms.L; .1]; % [phi phid xh xhd] a sample initial state
 [t,state,Ekin,Wtot] = bird_stance(x0, parms);
 
 phi=state(:,1);
@@ -38,7 +38,7 @@ plot(t,state(:,1:2))
 figure;
 for iStep=1:length(t)
     r_p=parms.L*[cos(phi(iStep)) sin(phi(iStep))];
-    plot([0 r_p(1)],[0 r_p(2)],'k',xh(iStep),yh,'ro'); hold on
+    plot([0 r_p(1)],[0 r_p(2)],'k',xh(iStep),yh,'ro',[r_p(1) xh(iStep)],[r_p(2) yh],'b--'); hold on
     drawnow
     axis equal
 end
